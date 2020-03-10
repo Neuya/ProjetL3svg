bdddb8e486a8f5386cdc68690f1da3a034a0e9c0
Paviel Raphael <raphael.paviel@etu.univ-orleans.fr>
2020-03-10 22:52:26 +0100

    Animation des elements terminée

--- premierProg/test.cpp
+++ premierProg/test.cpp
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <string>
+#include <gtk/gtk.h>
+#include <fstream>
+#include <map>
 
 
 using namespace tinyxml2;
@@ -14,42 +18,104 @@ using namespace std;
 static void do_drawing(cairo_t *);
 static void do_drawing_svg(cairo_t *);
 
+string element = "sun_x";
+//const char* new_pos = "100";
+
+
+
 
 RsvgHandle *svg_handle;
 int x = 10;
 int y = 0;
 gpointer user_data;
+GtkWidget *darea;
 // RsvgRectangle viewport;
+
+static void receive_msg(string e, string n)
+{
+  element = e;
+  //new_pos = n.c_str();
+}
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data)
 {
   user_data = user_data;
+
   do_drawing_svg(cr);
 
   return FALSE;
 }
 
+
+
 static void do_drawing_svg(cairo_t * cr)
 {
+
   XMLPrinter printer;
   XMLDocument svg_data;
   svg_data.LoadFile("toto.svg");
   XMLElement* xmlElement = svg_data.RootElement();
-  for (size_t i = 0; i < 12; i++) {
-    string s(xmlElement[i].Value());
-    std::cout << xmlElement[i].Value()<< '\n';
-    if (s == "circle") {
-      std::cout << "test"<< '\n';
-      xmlElement[i].SetAttribute("cx",x+=1);
-      xmlElement[i].SetAttribute("cy",y+=1);
-      svg_data.SaveFile("toto.svg",false);
-    }
+
+  map<string,string> mapModifs;
+  mapModifs["sun_x"]="20";
+  mapModifs["sun_y"]="80";
+  mapModifs["house_x"]="60";
+  mapModifs["house_y"]="65";
+
+  const char* target_x = "cx";
+  const char* target_y = "cy";
+  string line;
+
+  map<string,string>::iterator it;
+
+  int val1 = 0;
+  int val2 = 0;
+  for(it = mapModifs.begin();it!=mapModifs.end();it++)
+  {
+
+    for (size_t i=0; i<14; i++)
+    {
+      string s(xmlElement[i].Value());
+        if (s == "driven" &&  (xmlElement[i].FirstAttribute()->Next()->Value() == it->first ))
+        {
+          string current_pos;
+          const char* new_pos= it->second.c_str();
+
+          val2 = atoi(new_pos);
+
+
+
+
+            const char* attribute = xmlElement[i].FirstAttribute()->Value();
+            cout<<"attribute "<<attribute<<endl;
+
+            current_pos = xmlElement[i].Parent()->ToElement()->Attribute(attribute);
+
+
+            val1 = stoi(current_pos);
+            cout << " current_ posq : " << current_pos << " x new_pos : " << new_pos << endl;
+            if (! (val1 == val2))
+            {
+              val1 > val2? val1--:val1++;
+                  xmlElement[i].Parent()->ToElement()->SetAttribute(attribute,val1);
+            }
+
+
+          svg_data.SaveFile("toto.svg",false);
+
+      }
+
+      svg_data.Print(&printer);
+
+      svg_handle = rsvg_handle_new_from_data ((const unsigned char*) printer.CStr(), printer.CStrSize()-1, NULL);
+
+      rsvg_handle_render_cairo(svg_handle, cr);
+
+    gtk_widget_queue_draw(darea);
   }
-  svg_data.Print(&printer);
-  svg_handle = rsvg_handle_new_from_data ((const unsigned char*) printer.CStr(), printer.CStrSize()-1, NULL);
 
-  rsvg_handle_render_cairo(svg_handle, cr);
 
+}
 }
 
 
@@ -58,21 +124,25 @@ static void do_drawing_svg(cairo_t * cr)
 
 int main(int argc, char *argv[])
 {
+  // string c1 = "sun_x";
+  // string c2 = "100";
+  // map<string,string> m;
+  // m.insert(pair<string,string>(c1,c2));
+  //
+  // receive_msg(m.begin()->first, m.begin()->second);
   gtk_init(&argc, &argv);
   GtkWidget *window;
-  GtkWidget *darea;
+
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   darea = gtk_drawing_area_new();
   gtk_container_add(GTK_CONTAINER(window), darea);
-  gtk_widget_set_events (darea, GDK_EXPOSURE_MASK
-             | GDK_LEAVE_NOTIFY_MASK
-             | GDK_KEY_PRESS_MASK
-             | GDK_POINTER_MOTION_MASK
-             | GDK_POINTER_MOTION_HINT_MASK);
 
-  //fonction permettant d'afficher l'image SVG
-   g_signal_connect(darea, "draw",
-       G_CALLBACK(on_draw_event), NULL);
+
+  g_signal_connect(darea, "draw",
+    G_CALLBACK(on_draw_event), NULL);
+
+
+
 
 
    g_signal_connect(window, "destroy",
@@ -86,6 +156,7 @@ int main(int argc, char *argv[])
   gtk_widget_show_all(window);
 
   gtk_main();
+
 
   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
